27a7adfdfd8a46d58475e38b51f9cf67e8147443
Elias <eliassaso@gmail.com>
2023-05-29 12:32:03 -0600

    Add project files.

---
+++ WebApplication4NetFrame/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4NetFrame.Models;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WebApplication4NetFrame.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var studentIds = new string[5] { "Pedro", "Pablo", "Juan", "Antonio", "Carlos" };
+            ViewBag.Message = studentIds[1];
+            Result test = new Result();
+            test.value = studentIds[0];
+            return View(test);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+
+}
