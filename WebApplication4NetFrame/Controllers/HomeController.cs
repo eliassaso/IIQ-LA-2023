aa2397dd89b04a83d0e9af835311f92a06993ddc
Elias <eliassaso@gmail.com>
2023-05-30 20:26:22 -0600

    Test new branch

--- WebApplication4NetFrame/Controllers/HomeController.cs
+++ WebApplication4NetFrame/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace WebApplication4NetFrame.Controllers
     {
         public ActionResult Index()
         {
-            var studentIds = new string[5] { "Pedro", "Pablo", "Juan", "Antonio", "Carlos" };
+            var studentIds = new string[5] { "Elías", "Pablo", "Juan", "Antonio", "Carlos" };
             ViewBag.Message = studentIds[1];
             Result test = new Result();
             test.value = studentIds[0];
