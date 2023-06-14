4272f220e6dd0c04b08fa54590613495f947c970
Elias <eliassaso@gmail.com>
2023-06-13 20:17:49 -0600

    pre-quiz

---
+++ WebApplication4NetFrame/Models/Config.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4NetFrame.Models
+{
+    public static class Config
+    {
+        private static string connectionString = "server=saacapps.com;UserID=saacapps_ucatolica;Database=saacapps_training;Password=Ucat0lica";
+
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+
+    }
+}
